07cad41d0619863f345597e43cbc57b5ab677fa1
João Gomes <joaoeduardo.gomes21@gmail.com>
2021-12-29 15:40:16 -0300

    grammar problem

--- perguntas-respotas.py
+++ perguntas-respotas.py
@@ -6,7 +6,7 @@ def filmes(num):
                 2: f'Um {tema} para ver com um par romântico.', 
                 3: f'O pior {tema} que você já assistiu.', 
                 4: f'Seu {tema} favorito de todos os tempos.', 
-                5: f'Você busca assistir {tema}s indicados a premiações? Se sim, por quê?', 
+                5: f'Você busca assistir {tema}s indicados em premiações? Se sim, por quê?', 
                 6: f'Prêmios são bons indicadores da qualidade de um {tema}?', 
                 7: f'O que você gosta de comer/beber enquanto assiste a um {tema}?', 
                 8: f'Qual o tempo de duração aceitável de um {tema}. Quando ele passa a ser longo demais?', 
