837bc8e5a7d599aee4a9eb921d2134bd2c086b35
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-01-15 19:38:16 -0300

    "vs_jogos" almost done!

--- vs_jogos.py
+++ vs_jogos.py
@@ -1,33 +1,45 @@
-from random import randint
+from random import randrange
 
-jogos = {
-        1: "The Last of Us",
-        2: "The Last of US Part II",
-        3: "Life is Strange",
-        4: "Firewatch",
-        5: "Persona 5",
-        6: "Breath of the Wild",
-        7: "It Takes Two",
-        9: "Pokémon Soul Silver",
-        10: "The Witcher 3",
-        11: "Grim Fandango",
-        12: "Tales From The Borderlands"
-        }
-
-
-a = randint(1, len(jogos))
-b = randint(1, len(jogos))
-print(f"\n")
-print(f"{jogos[a]} vs {jogos[b]}")
+jogos = [
+        "The Last of Us",
+        "The Last of US Part II",
+        "Life is Strange",
+        "Firewatch",
+        "Persona 5",
+        "Breath of the Wild",
+        "It Takes Two",
+        "God of War",
+        "Pokémon Soul Silver",
+        "The Witcher 3",
+        "Grim Fandango",
+        "Tales From The Borderlands"
+    ]
 
 
-# Finalistas
-lista = ["Limbo", "Journey", "Rain"]
-numeros = [1, 2, 3]
-dicionario = dict(zip(numeros, lista))
-print(dicionario)
+while len(jogos) > 1:
 
-num = []
-for i in range(1, len(lista)+1):
-    num.append(i)
-print(num)
+    a = randrange(0, len(jogos))
+
+    b = randrange(0, len(jogos))
+    if b == a:
+        while b == a:
+            b = randrange(0, len(jogos))
+        
+    print(f"\n")
+    print(f"{jogos[a]} vs {jogos[b]}")
+
+    while True:
+        try:
+            escolha = int(input("Qual dos dois você prefere? "))
+            break
+        except ValueError:
+            continue
+    if escolha == 1:
+        jogos.pop(b)
+    elif escolha == 2:
+        jogos.pop(a)
+
+    print(jogos)
+
+
+print(f"O vencedor é: {jogos}")
